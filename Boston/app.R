e04b3768686ed5b2a607112a02077f242d4bb80c
xiaofanxia <xiaofx@uw.edu>
2019-10-27 12:24:30 -0400

    update

--- Boston/app.R
+++ Boston/app.R
@@ -9,21 +9,3 @@
 
 library(shiny)
 
-pacman::p_load("ggmap","maptools","maps","mapproj","shiny")
-worldmap <- map_data("world")
-projections <- c("cylindrical","mercator","sinusoidal","gnomonic","rectangular","cylequalarea")
-
-ui <- fluidPage(
-    titlePanel("World Map"),
-    
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("p","Select a Projection",choices = projections),
-            sliderInput("rng1", "Range for longitude", value = c(-180,180),min=-200,max=200),
-            sliderInput("rng2", "Range for latitude", value = c(-60,90),min=-100,max=100)
-        ),
-        mainPanel(
-            plotOutput("mymap")
-        )
-    )
-)
